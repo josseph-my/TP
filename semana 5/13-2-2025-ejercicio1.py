c56eb203b052f3f7849fc87c77b8c69888d1a653
josseph-my <samuelcordoba999@gmail.com>
2025-02-13 11:22:47 -0600

    ejercicios semana 5
    control, for, iteraria

---
+++ semana 5/13-2-2025-ejercicio1.py
@@ -0,0 +1,5 @@
+print("hello world")
+varA = " Hello "
+varB = varA * 3
+print(varB)
+
---
+++ semana 5/13-2-2025-for.py
@@ -0,0 +1,16 @@
+
+
+for i in range(5):
+    print(i)
+print("*********************")
+print("         for1            ")
+
+for i in range(10):
+    print(i)
+print("*********************")
+print("         for2            ")
+
+for i in range(11):
+    print(i)
+print("*********************")
+print("         for3            ")
